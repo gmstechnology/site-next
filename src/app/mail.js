56789354674ad63b0cb23ce74cd6a7fd7aae2533
lchirata <larissa.c.hirata@gmail.com>
2024-05-23 22:15:11 -0300

    promisse in send email

--- src/app/mail.js
+++ src/app/mail.js
@@ -11,31 +11,39 @@ export async function sendMail(data){
             pass: SMTP_PASSWORD
         }
     })
-    try{
-        const testResult = await transport.verify()
-        console.log("testResult")
-        console.log(testResult)
-    }catch(err){
-        console.log(err)
-        return
-    }
-    try{
-        const sendResult = await transport.sendMail({
-            from: SMTP_EMAIL,
-            to: ["larissa.c.hirata@gmail.com", "henrique.h@gmstechnology.com.br", "erick.h@gmstechnology.com.br"],
-            subject: `${data.name} mostrou interesse no equipamento ${data.eqName}`,
-            html:
-                `<h4> Mandar email para ${data.name} - ${data.email} sobre o equipamento: ${data.eqName} </h4>
-                    <br />
-                 <p> ${data.name} escreveu: ${data.msg}</p>
-                `
 
-        })
-        console.log("sendResult")
-        console.log(sendResult)
-        return true
-    }catch(err){
-        console.log(err)
-    }
+    await new Promise((resolve, reject) => {
+        // verify connection configuration
+        transport.verify(function (error, success) {
+            if (error) {
+                console.log(error);
+                reject(error);
+            } else {
+                console.log("Server is ready to take our messages");
+                resolve(success);
+            }
+        });
+    })
 
+    await new Promise((resolve, reject) => {
+
+    let mailData = {
+                from: SMTP_EMAIL,
+                to: ["larissa.c.hirata@gmail.com"],// "henrique.h@gmstechnology.com.br", "erick.h@gmstechnology.com.br"
+                subject: `${data.name} mostrou interesse no equipamento ${data.eqName}`,
+                html: 
+                    `<h4> Mandar email para ${data.name} - ${data.email} sobre o equipamento: ${data.eqName} </h4>
+                        <br />
+                    <p> ${data.name} escreveu: ${data.msg}</p>
+                    `}
+    transport.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.error(err);
+            reject(err);
+        } else {
+            console.log(info);
+            resolve(info);
+        }
+    })
+    })
 }
